5b7f38eeaf1d0e6468d64b3c166968f99d0e924d
FireBall1725 <treed@sunnking.com>
2014-12-14 18:23:35 -0500

    fixed mcmod.info

--- src/main/java/com/fireball1725/defaultworldgenerator/lib/Reference.java
+++ src/main/java/com/fireball1725/defaultworldgenerator/lib/Reference.java
@@ -1,11 +1,8 @@
 package com.fireball1725.defaultworldgenerator.lib;
 
 public class Reference {
-    public static final String MOD_ID = "defaultworldtype";
-    public static final String MOD_NAME = "Default World Type";
+    public static final String MOD_ID = "defaultworldgenerator";
+    public static final String MOD_NAME = "Default World Generator";
     public static final String VERSION_BUILD = "@VERSION@";
     public static final String VERSION_BUILD_NUMBER = "@BUILDNUMBER@";
-    public static final String SERVER_PROXY_CLASS = "com.fireball1725.twitchnotifier.proxy.ServerProxy";
-    public static final String CLIENT_PROXY_CLASS = "com.fireball1725.twitchnotifier.proxy.ClientProxy";
-    public static final String GUI_FACTORY = "com.fireball1725.twitchnotifier.config.TwitchNotifierConfigGuiFactory";
 }
